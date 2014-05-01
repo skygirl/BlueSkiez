abfb4adf12e24dc1e4752fed4050ac9679c909d3
Maytee Kneitz <maytee.kneitz@gmail.com>
2014-04-30 21:23:56 -0500

    Seeded DB and added contact form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Customer.create(cust_fname:"John", cust_lname:"Kneitz", username:"abc123", password_digest:"abc12323232", cust_city:"Miami", cust_addr:"123 abc st", cust_em_contact:"gfgfdhg@aol.com",cust_email:"abc23@123.com", cust_zip:"60014", cust_phone:"815-654-1234", cust_em_contact:"hsgsg@aol.com", cust_em_contactrel:"Spouse", cust_em_contactphone:"716-817-5454", cust_dl:"61516161616", cust_dl_state:"IL", cust_dl_exp:"1/2014", cust_status:"Sport", cust_cc:"123456788", cust_waiver_date: "1/1/2014", cust_id:"1")
